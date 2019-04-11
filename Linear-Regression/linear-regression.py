096161a9ab673e435404b289769db17392176de0
Pradeesh Bm <pradeeshbm@ymail.com>
2019-04-11 21:16:26 +0530

    Added dataset

--- Linear-Regression/linear-regression.py
+++ Linear-Regression/linear-regression.py
@@ -1,2 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Linear Regression
+
+The dataset for this model has been downloaded from Kaggle - https://www.kaggle.com/harlfoxem/housesalesprediction
+
+@author: Pradeesh
+"""
+
 import numpy as np
 import pandas as pd
+
+# Load dataset
